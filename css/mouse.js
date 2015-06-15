fb0dba291e95c2b65e69164ece45014b96a47411
linfanl <linfann@gmail.com>
2015-06-15 02:26:54 -0700

    sliding menu is implemented

---
+++ css/mouse.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+function onClick(){
+	
+	document.getElementById('sub-menu').style.cssText='height:100px; transition:all 0.4s ease-in-out';
+	
+
+}
+
+function onmouseOut(){
+	
+	document.getElementById('sub-menu').style.cssText='height:0px;transition:all 0.4s ease-in-out';
+	
+
+}
