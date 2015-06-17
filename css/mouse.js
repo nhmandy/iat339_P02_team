c6aed96b692d5f80aeea68cd0fcf43d0d5f1e878
linfanl <linfann@gmail.com>
2015-06-16 19:42:34 -0700

    added in sign-up and log-in button, pop-up window

--- css/mouse.js
+++ css/mouse.js
@@ -4,12 +4,40 @@ function onClick(){
 	
 	document.getElementById('sub-menu').style.cssText='height:100px; transition:all 0.4s ease-in-out';
 	
+	
 
 }
 
 function onmouseOut(){
 	
 	document.getElementById('sub-menu').style.cssText='height:0px;transition:all 0.4s ease-in-out';
+	
+
+}
+
+
+function loginPopup(){
+	
+	document.getElementById('log-in-page').style.cssText='display:block';
+	
+
+}
+function loginClose(){
+	
+	document.getElementById('log-in-page').style.cssText='display:none';
+	
+
+}
+
+function signupPopup(){
+	
+	document.getElementById('sign-up-page').style.cssText='display:block';
+	
+
+}
+function signupClose(){
+	
+	document.getElementById('sign-up-page').style.cssText='display:none';
 	
 
 }
