efa99b28fcdde6537843063668eb90297c778baa
linfanl <linfann@gmail.com>
2015-06-29 02:07:43 -0700

    New folder created, used grid and re-organized all pages. Currently only the index page works responsively, still need to work on the other pages

---
+++ vera's_florist_design/js/event.js
@@ -0,0 +1,62 @@
+// JavaScript Document
+function signupOpen(){
+	
+	document.getElementById('sign-up-wrapper').style.cssText='visibility:visible; opacity:1; transition:all 0.2s ease-in-out';
+	
+	
+
+}
+function signupClose(){
+	
+	document.getElementById('sign-up-wrapper').style.cssText='visibility:hidden; opacity:0; transition:all 0.2s ease-in-out';
+	
+	
+}
+
+function loginOpen(){
+	
+	document.getElementById('log-in-wrapper').style.cssText='visibility:visible; opacity:1; transition:all 0.2s ease-in-out';
+	
+	
+
+}
+
+function loginClose(){
+	
+	document.getElementById('log-in-wrapper').style.cssText='visibility:hidden; opacity:0; transition:all 0.2s ease-in-out';
+	
+	
+}
+
+
+function submenuOpen(){
+	
+	document.getElementById('sub-menu').style.cssText='height:100px; transition:all 0.4s ease-in-out';
+	
+	
+
+}
+
+function submenuClose(){
+	
+	document.getElementById('sub-menu').style.cssText='height:0; transition:all 0.4s ease-in-out';
+	
+
+}
+
+function billOpen(){
+	
+	document.getElementById('check-out-box').style.cssText='visibility:visible; opacity:1; transition:all 0.2s ease-in-out';
+	
+	
+
+}
+
+
+function billClose(){
+	
+	document.getElementById('check-out-box').style.cssText='visibility:hidden; opacity:0; transition:all 0.2s ease-in-out';
+	
+	
+
+}
